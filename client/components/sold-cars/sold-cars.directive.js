797c37a23722d71374fb6fe6cdf4fc292f07b803
Elena Romanets <elena@ebuilders.nl>
2015-12-04 10:49:26 +0200

    Sharing stuff

--- client/components/sold-cars/sold-cars.directive.js
+++ client/components/sold-cars/sold-cars.directive.js
@@ -6,6 +6,16 @@ angular.module('marktplaatsIacVerkoopApp')
       templateUrl: 'components/sold-cars/sold-cars.html',
       restrict: 'EA',
       link: function (scope, element, attrs) {
+        element.find(".icon-social-twitter").each(function () {
+          var wrapper = $(this).closest('.row');
+          var description = wrapper.find('.sold-cars__descr').text();
+          var encodedDescription = encodeURIComponent(description);
+          var videoId = wrapper.find('sold-cars-item').attr('youtube-id');
+
+          var link = 'https://twitter.com/share?url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D' + videoId + '&text=' + encodedDescription + '&hashtags=Marktplaats';
+          $(this).attr("href", link);
+        })
       }
     };
   });
+
